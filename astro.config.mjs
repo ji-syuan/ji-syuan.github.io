1f46e34ca2a5dba4da1eb733c0e9f40015663d12
Yasmine-Cheng <mian12080916@gmail.com>
2025-05-08 14:09:30 +0800

    <FIX> github page setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     output: 'static',
     trailingSlash: 'always',
     site: 'https://ji-syuan.github.io',
-    base: 'ji-syuan.github.io',
+    base: '/',
 
     // Single page, no prefetch needed
     prefetch: false,
